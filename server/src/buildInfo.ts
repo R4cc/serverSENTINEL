814786c4109bbf1df4ef8599843e637111f7c3f8
R4cc <niklas.lerchenmueller59@gmail.com>
2026-08-24 18:10:15 +0200

    Align application version metadata to 26.8.18

--- server/src/buildInfo.ts
+++ server/src/buildInfo.ts
@@ -1,4 +1,4 @@
-export const appVersion = process.env.npm_package_version ?? "26.8.32";
+export const appVersion = process.env.npm_package_version ?? "26.8.18";
 
 export function appUserAgentFor(component: string) {
   return `serverSENTINEL/${appVersion} (${component}; https://github.com/R4cc/serverSENTINEL)`;
